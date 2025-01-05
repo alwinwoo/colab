11f56dd8c606a74755fc7c50dda4b6fb427eac07
Alwin Woo <alwinwoo@users.noreply.github.com>
2025-01-05 17:19:01 +0800

    Update common_modules.py

--- common_modules.py
+++ common_modules.py
@@ -19,9 +19,9 @@ try:
     except Exception as e:
         try:
             if "web" in use_category:
-                !pip install beautifulsoup4
+                pip install beautifulsoup4
             if "ai" in use_category:
-                !pip install scikit-learn tensorflow
+                pip install scikit-learn tensorflow
         except Exception as e:
             print(f'Error loading special modules: {e}')
             
