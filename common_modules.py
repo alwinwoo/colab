3635e9cc05923667f1b041a91e02a89ee5fa3cd5
Alwin Woo <alwinwoo@users.noreply.github.com>
2025-01-05 17:17:49 +0800

    Update common_modules.py

--- common_modules.py
+++ common_modules.py
@@ -1,30 +1,29 @@
-def load_common_modules(use_category):
+try:
+    import os, sys, re, json, itertools, subprocess
+    import time, datetime, pytz
+    import math, random
+    import logging
+    import csv, numpy, pandas, matplotlib
+    
     try:
-        import os, sys, re, json, itertools, subprocess
-        import time, datetime, pytz
-        import math, random
-        import logging
-        import csv, numpy, pandas, matplotlib
-    
-        try:
-            if "web" in use_category:
-                import requests, beautifulsoup4
-                print('Importing web modules')
+        if "web" in use_category:
+            import requests, beautifulsoup4
+            print('Importing web modules')
                 
-            if "ai" in use_category:
-                import sklearn, tensorflow
-                print('Importing AI modules')
+        if "ai" in use_category:
+            import sklearn, tensorflow
+            print('Importing AI modules')
     
-            print('Special modules loaded')
-            
-        except Exception as e:
-            try:
-                if "web" in use_category:
-                    !pip install beautifulsoup4
-                if "ai" in use_category:
-                    !pip install scikit-learn tensorflow
-            except Exception as e:
-                print(f'Error loading special modules: {e}')
+        print('Special modules loaded')
             
     except Exception as e:
-        print(f'Error loading common modules: {e}')
+        try:
+            if "web" in use_category:
+                !pip install beautifulsoup4
+            if "ai" in use_category:
+                !pip install scikit-learn tensorflow
+        except Exception as e:
+            print(f'Error loading special modules: {e}')
+            
+except Exception as e:
+    print(f'Error loading common modules: {e}')
