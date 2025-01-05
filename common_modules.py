ad628124a0d9746823bd1264cef605ca460e1958
Alwin Woo <alwinwoo@users.noreply.github.com>
2025-01-05 16:25:01 +0800

    Update common_modules.py

--- common_modules.py
+++ common_modules.py
@@ -1,20 +1,21 @@
-try:
-    import os, sys, math, datetime, re, json, random, time, itertools, subprocess, logging
-    import csv, numpy, pandas, matplotlib
-
+def import_common_modules(use_category):
     try:
-        if "web" in use_category:
-            import requests, beautifulsoup4
-            print('Importing web modules')
+        import os, sys, math, datetime, re, json, random, time, itertools, subprocess, logging
+        import csv, numpy, pandas, matplotlib
+    
+        try:
+            if "web" in use_category:
+                import requests, beautifulsoup4
+                print('Importing web modules')
+                
+            if "ai" in use_category:
+                import sklearn, tensorflow
+                print('Importing AI modules')
+    
+            print('Special modules loaded')
             
-        if "ai" in use_category:
-            import sklearn, tensorflow
-            print('Importing AI modules')
-
-        print('Special modules loaded')
-        
+        except Exception as e:
+            print(f'Error loading special modules: {e}')
+            
     except Exception as e:
-        print(f'Error loading special modules: {e}')
-        
-except Exception as e:
-    print(f'Error loading common modules: {e}')
+        print(f'Error loading common modules: {e}')
