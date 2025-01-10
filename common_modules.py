d7eaa29ef938e3c0d47880ba95ca77e5ed393ce4
Alwin Woo <alwinwoo@users.noreply.github.com>
2025-01-10 17:57:41 +0800

    Update common_modules.py

--- common_modules.py
+++ common_modules.py
@@ -1,32 +1,12 @@
 try:
-    import os, sys, re, json, itertools, subprocess
-    import time, datetime
-    import math, random
-    import logging
-    import csv, numpy, pandas, matplotlib
+	import os, sys, re, itertools, subprocess
+	import time, datetime
+	import logging
 
-    os.system('pip install pytz')
-    import pytz
-    
-    try:
-        if "web" in use_category:
-            import requests, beautifulsoup4
-            print('Importing web modules')
-                
-        if "ai" in use_category:
-            import sklearn, tensorflow
-            print('Importing AI modules')
-    
-        print('Special modules loaded')
-            
-    except Exception as e:
-        try:
-            if "web" in use_category:
-                os.system('pip install beautifulsoup4')
-            if "ai" in use_category:
-                os.system('pip install scikit-learn tensorflow')
-        except Exception as e:
-            print(f'Error loading special modules: {e}')
-            
+	import math, random
+
+	import numpy, pandas, matplotlib
+	import requests, json, csv
+
 except Exception as e:
-    print(f'Error loading common modules: {e}')
+	print(f'Error loading common modules: {e}')	
